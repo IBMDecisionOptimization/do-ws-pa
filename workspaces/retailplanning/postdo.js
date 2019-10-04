f6f1c674ef7e6c94f4bb0eab3acd213cf4862341
Alain Chabrier <38245448+achabrier@users.noreply.github.com>
2019-10-04 17:38:15 +0200

    workspaces

--- workspaces/retailplanning/postdo.js
+++ workspaces/retailplanning/postdo.js
@@ -14,22 +14,44 @@ let revenue_cols = ['CustomerID', 'OfferID', 'ChannelID', 'expectedrevenue'];
 let revenues = scenario.addTable('expectedrevenue', 'output', revenue_cols, {});
 
 let candidates = scenario.getTableRows('Candidate');
-let selecteds = scenario.getTableRows('selected');
+
+let mycandidates = {}
 
 for (let i in candidates) {
     let candidate = candidates[i];
+    let customer = candidate['CustomerID'];
+    let offer =  candidate['OfferID']
+    let channel = candidate['ChannelID'];
+    if (!(customer in mycandidates))
+        mycandidates[customer] = {}
+    if (!(offer in mycandidates[customer]))
+        mycandidates[customer][offer] = {}
+    mycandidates[customer][offer][channel] = candidate;
+}
+
+
+let selecteds = scenario.getTableRows('selected');
+
+let j = 0;
+for (let i in candidates) {
+
     let selected = selecteds[i];
     
     if (selected['selected'] == 0)
         continue;
 
+    let customer = selected['CustomerID'];
+    let offer =  selected['OfferID']
+    let channel = selected['ChannelID'];
+    let candidate = mycandidates[customer][offer][channel];
+    
     let cost_row = {}
     cost_row['CustomerID'] = selected['CustomerID'];
     cost_row['OfferID'] = selected['OfferID']
     cost_row['ChannelID'] = selected['ChannelID'];
     cost_row['cost'] = candidate['Cost'];
     
-    scenario.addRowToTable('cost', i, cost_row);    
+    scenario.addRowToTable('cost', j, cost_row);    
 
     let revenue_row = {}
     revenue_row['CustomerID'] = selected['CustomerID'];
@@ -37,6 +59,7 @@ for (let i in candidates) {
     revenue_row['ChannelID'] = selected['ChannelID'];
     revenue_row['expectedrevenue'] = candidate['ExpectedRevenue'];
     
-    scenario.addRowToTable('expectedrevenue', i, revenue_row);  
+    scenario.addRowToTable('expectedrevenue', j, revenue_row);  
+    j += 1;
 }
 
